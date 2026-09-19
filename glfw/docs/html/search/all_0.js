8bdeebc29e5998ddc413836f71b3a0458b6832a8
sysdctl <radesorkh@gmail.com>
2026-09-19 22:36:02 +0330

    switch to FetchContent for GLFW and ImGui

--- glfw/docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['0_20release_0',['Why was there no 3.5.0 release?',['../news.html#autotoc_md0',1,'']]]
-];
--- glfw/docs/html/search/all_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['5_0',['Release notes for version 3.5',['../news.html',1,'']]],
-  ['5_200_20release_1',['Why was there no 3.5.0 release?',['../news.html#autotoc_md0',1,'']]]
-];
