6e4e75bf870dadd49512352c42eacd19d58643d8
sysdctl <radesorkh@gmail.com>
2026-09-20 08:41:16 +0330

    setup full ImGui application with GLFW + OpenGL backend

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,104 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
 #include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
-#include <GLFW/glfw3.h>
+
 
 int main ()
 {
+        // -------------------------
+        // GLFW
+        // -------------------------
+
+        if (!glfwInit())
+        {
+                std::cerr << "Failed to initialize GLFW\n";
+                glfwTerminate();
+                return 1;
+        }
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        GLFWwindow* window = glfwCreateWindow(1280, 720, "My ImGui Project", nullptr, nullptr);
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        // -------------------------
+        // Dear ImGui
+        // -------------------------
+
+        IMGUI_CHECKVERSION();
+        ImGuiIO& io = ImGui::GetIO();
+        (void)io;
         ImGui::CreateContext();
-        ImGui::NewFrame();
-        ImGui::Begin("My First Window");
 
-        ImGui::Text("Hello Dear ImGui!");
+        ImGui::StyleColorsDark();
 
-        ImGui::End();
-        ImGui::Render();
+        // -------------------------
+        // ImGui Backends
+        // -------------------------
+
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 330");
+
+        // -------------------------
+        // Main Loop
+        // -------------------------
+
+        while (!glfwWindowShouldClose(window))
+        {
+                glfwPollEvents();
+
+                ImGui_ImplOpenGL3_NewFrame();
+                ImGui_ImplGlfw_NewFrame();
+                ImGui::NewFrame();
+
+                // -------------------------// GUI // ------------------------- //
+
+                ImGui::SetNextWindowPos(ImVec2(0, 0));
+                ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+                ImGui::Begin(
+                        "My First Window",
+                        nullptr,
+                        ImGuiWindowFlags_NoTitleBar |
+                        ImGuiWindowFlags_NoResize |
+                        ImGuiWindowFlags_NoMove
+                        );
+
+                // ---- Elements ---- //
+
+                
+                ImGui::Text("Hello Dear ImGui!");
+
+                if (ImGui::Button("Click Me"))
+                {
+                std::cout << "Button clicked!\n";
+                }
+
+
+                // -------------------------// Render // ------------------------- //
+
+                ImGui::End();
+
+                ImGui::Render();
+                
+                ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+                glfwSwapBuffers(window);
+
+        }
+
+        // -------------------------
+        // Cleanup
+        // -------------------------
+
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
         ImGui::DestroyContext();
+        glfwDestroyWindow(window);
+        glfwTerminate();
+
+        return 0;
 }
